e3c2a19517c67c3d61833f2a640c3fbce124a458
younjaewon <97930034+younjaewon@users.noreply.github.com>
2022-02-01 11:08:00 +0900

    메뉴 수정 및 코드리팩터링

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,66 @@
+const $ = (selector) => document.querySelector(selector);
+
 function App() {
+  $("#espresso-menu-list").addEventListener("click", (e) => {
+    if (e.target.classList.contains("menu-edit-button")) {
+      const $menuName = e.target.closest("li").querySelector(".menu-name");
+      const updatedMenuName = prompt("메뉴명을 수정해주세요.", $menuName.innerText);
+      $menuName.innerText = updatedMenuName;
+    }
+  })
   // form 태그 자동 전송 되는걸 막는다.
-  document.querySelector("#espresso-menu-form").addEventListener("submit",
+  $("#espresso-menu-form").addEventListener("submit",
     (e) => {
       e.preventDefault();
     });
+  
+  const addMenuNameListner = () => {
+    if ($("#espresso-menu-name").value === "") {
+      alert("값을 입력해주세요.");
+      return;
+    }
+
+    const espressoMenuName = $("#espresso-menu-name").value;
+    const menuItemTemplate = (espressoMenuName) => {
+      return ` <li class="menu-list-item d-flex items-center py-2">
+          <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+          >
+            수정
+          </button>
+          <button
+            type="button"
+            class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+          >
+            삭제
+          </button>
+        </li> `;
+    };
+
+    $("#espresso-menu-list").insertAdjacentHTML(
+      "beforeend",
+      menuItemTemplate(espressoMenuName)
+    );
+    // count 변수 = li 갯수 카운트
+    const menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+    $(".menu-count").innerText = `총 ${menuCount} 개`;
+
+    $("#espresso-menu-name").value = "";
+  };
+  
+  $("#espresso-menu-submit-button").addEventListener("click", () => {
+    addMenuNameListner();
+  });
 
   // 1. 메뉴의 이름을 입력받는다.
-  document.querySelector("#espresso-menu-name").addEventListener("keypress",
-    (e) => {
-      console.log(e.key);
-      if (e.key === "Enter") {
-        
-      }
+  $("#espresso-menu-name").addEventListener("keypress", (e) => {
+    if (e.key !== "Enter") {
+      return;
+    }
+
+    addMenuNameListner();
   });
 }
 
